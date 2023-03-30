f8fc3dea54272a00fbe924c434fc01c3c347916a
erionamucolli <128907339+erionamucolli@users.noreply.github.com>
2023-03-30 21:11:46 +0200

    updated bucket acl

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "aws_s3_bucket" "bucket" {
     Environment = var.environment
   }
 }
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
